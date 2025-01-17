e2fe25381639e8974c8058d230780067441db54c
hlaporte24 <97315020+hlaporte24@users.noreply.github.com>
2025-01-17 14:15:14 -0600

    robot assignment -- tree

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,27 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
+		robot.forward(120);
+		robot.turnRight(90);
 		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(50);
+		robot.turnLeft(90);
+		robot.forward(200);
+		robot.turnLeft(90);
+		robot.forward(300);
+		robot.turnLeft(90);
+		robot.forward(200);
+		robot.turnLeft(90);
+		robot.forward(50);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(120);
 	}
 
 }
